0565690cdf15055a637ecc12f68852d72c8af25c
Yu.Zhang <714794053@qq.com>
2018-06-08 15:16:19 +0800

    zenjia

---
+++ oo.js
@@ -0,0 +1,13 @@
+tpwidget("init", {
+    "flavor": "bubble",
+    "location": "WS0E9D8WN298",
+    "geolocation": "enabled",
+    "position": "top-right",
+    "margin": "10px 10px",
+    "language": "zh-chs",
+    "unit": "c",
+    "theme": "chameleon",
+    "uid": "U6A7695174",
+    "hash": "b480a07f6d79ab4df9168d52bf9bf9b7"
+});
+tpwidget("show");
